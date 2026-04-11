ff5eb933f9ef5eef9ccbad492627a0630f2556aa
skxk774 <skxk774@gmail.com>
2026-04-11 22:52:48 +0900

    add icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// регистрация PWA
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
