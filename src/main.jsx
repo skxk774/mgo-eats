b38e72b5827e4da1a8149abd812c95da8bb01d67
skxk774 <skxk774@gmail.com>
2026-04-14 22:33:58 +0900

    remove pwa from main

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// регистрация PWA
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW()
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
